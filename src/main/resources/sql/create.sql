88f0a34df331a95a94f6a01671e55561d55929e3
songsunkook <songsunkook@gmail.com>
2020-12-29 17:02:04 +0900

    .

---
+++ src/main/resources/sql/create.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `beginner_project`.`user` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `account_id` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `nickname` VARCHAR(50) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `account_id_UNIQUE` (`account_id` ASC) VISIBLE);
+
+CREATE TABLE `beginner_project`.`board` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(200) NOT NULL,
+  `content` VARCHAR(3000) NOT NULL,
+  `author` VARCHAR(50) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `beginner_project`.`comment` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `post_id` INT NOT NULL,
+  `content` VARCHAR(300) NOT NULL,
+  `author` VARCHAR(50) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`));
