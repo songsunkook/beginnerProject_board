f398bc5e859cfa5893f25d6edf44f8bdff1b7774
songsunkook <songsunkook@gmail.com>
2020-12-29 21:51:50 +0900

    .

---
+++ src/main/java/domain/Comment.java
@@ -0,0 +1,60 @@
+package domain;
+
+import java.sql.Timestamp;
+
+public class Comment {
+    protected Long id;
+    protected Long post_id;
+    protected String content;
+    protected String author;
+    protected Timestamp created_at;
+    protected Timestamp updated_at;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPost_id() {
+        return post_id;
+    }
+
+    public void setPost_id(Long post_id) {
+        this.post_id = post_id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+}
---
+++ src/main/java/domain/User.java
@@ -0,0 +1,51 @@
+package domain;
+
+import java.sql.Timestamp;
+
+public class User {
+    protected Long id;
+    protected String account_id;
+    protected String password;
+    protected String nickname;
+    protected Timestamp created_at;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(String account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+}
